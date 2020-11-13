40119cde3d1d81a5e00316325b6c915747b50ff4
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-11-12 19:35:16 -0700

    updated

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -3,17 +3,17 @@ import * as yup from "yup";
 export default yup.object().shape({
     name: yup
         .string()
-        .required("name is required")
-        .min(3, "username must be 3 chars long"),
+        .required("Name is required")
+        .min(3, "Name must be 3 chars long"),
     email: yup
         .string()
         .email("Must be valid email address")
         .required("Must include email address"),
     password: yup
         .string()
-        .required("password is required")
-        .min(3, "password must be 3 chars long"),
+        .required("Password is required")
+        .min(3, "Password must be 3 chars long"),
     terms: yup
         .boolean()
-        .required("must accept Terms of Service"),
+        .oneOf([true], "Must accept Terms of Service")
 });
